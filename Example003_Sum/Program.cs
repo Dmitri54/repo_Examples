b5e66539506d73c5d5f383ce323ae8e6b60e3817
Dmitri5 <dmitriprogram@gmail.com>
2022-06-29 16:24:25 +0700

    Added the option with a random number generator

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -17,10 +17,14 @@
 // int result = numberA + numberB; // Тоже самое, но через промежуточную переменную.
 // Console.WriteLine(result);
 // --------------------------------------------------------------------------------------
-
-int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9 или можно спросить у пользователя как в файле Hello User.
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10); 
+// Третий вариант с генератором случайных чисел.
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9 или можно спросить у пользователя как в файле Hello User.
+Console.WriteLine(numberA); // покажет первое число и перейдет на новую строку.
+int numberB = new Random().Next(1, 10); 
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
+
+// int numberA = new Random().Next(1,10); - что я объявляю новую переменную numberA,
+// в токорую записываю результат new Rendom().Next(1,10) - генератор случайных чисел, выдаст 
+// случайное число в у казанном диапазоне [1,10), т.е.невключая 10.
